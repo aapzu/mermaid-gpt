d64609bff3639dbf9f8f7ecb53b6e3afc29e45b4
Aapeli <aapzu@iki.fi>
2024-02-26 08:41:48 +0200

    Add option to disable auto compilation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,57 +1,20 @@
-import { Dispatch, FC, SetStateAction, useState } from 'react';
+import { Dispatch, FC, SetStateAction } from 'react';
 
 import { Settings, SettingsDialog } from './SettingsDialog';
-import {
-  NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-} from './ui/navigation-menu';
-import { Button } from './ui/button';
 
 type HeaderProps = {
   settings: Settings;
   setSettings: Dispatch<SetStateAction<Settings>>;
 };
 export const Header: FC<HeaderProps> = ({ settings, setSettings }) => {
-  const [settingsOpen, setSettingsOpen] = useState(false);
   return (
-    // <div className="container mx-auto px-4 md:px-6 lg:px-8">
-    //   <header className="flex h-20 w-full shrink-0 items-center px-4 md:px-6">
-    //     <NavigationMenu className="flex justify-between w-full">
-    //       <NavigationMenuList>
-    //         <NavigationMenuItem>
-    //           <h1 className="scroll-m-20 text-2xl font-extrabold tracking-tight lg:text-3xl">
-    //             MermaidGPT
-    //           </h1>
-    //         </NavigationMenuItem>
-    //       </NavigationMenuList>
-    //       <NavigationMenuList>
-    //         <NavigationMenuItem>
-    //           <Button onClick={() => setSettingsOpen(true)}>Settings</Button>
-    //           <SettingsDialog
-    //             isOpen={settingsOpen}
-    //             onClose={() => setSettingsOpen(false)}
-    //             settings={settings}
-    //             setSettings={setSettings}
-    //           />
-    //         </NavigationMenuItem>
-    //       </NavigationMenuList>
-    //     </NavigationMenu>
-    //   </header>
-    // </div>
     <div className="container mx-auto px-4 md:px-6 lg:px-8">
       <header className="flex h-20 w-full shrink-0 items-center px-4 md:px-6">
         <h1 className="scroll-m-20 text-2xl font-extrabold tracking-tight lg:text-3xl">
           MermaidGPT
         </h1>
         <div className="ml-auto flex gap-2">
-          <SettingsDialog
-            settings={settings}
-            setSettings={setSettings}
-            isOpen={settingsOpen}
-            onClose={() => setSettingsOpen(false)}
-          />
+          <SettingsDialog settings={settings} setSettings={setSettings} />
         </div>
       </header>
     </div>
--- src/components/LoadingBar/index.tsx
+++ src/components/LoadingBar/index.tsx
@@ -1,11 +1,12 @@
 import { FC } from 'react';
 
 import { cn } from '@/utils';
+import './styles.css';
 
 export const LoadingBar: FC<{ loading: boolean }> = ({ loading }) => {
   return (
     <div className={cn('w-full', { 'opacity-0': !loading })}>
-      <div className="h-1.5 -mt-1.5 w-full bg-pink-100 overflow-hidden">
+      <div className="h-0.5 -mt-0.5 w-full bg-pink-100 overflow-hidden">
         <div className="progress w-full h-full bg-pink-500 left-right"></div>
       </div>
     </div>
