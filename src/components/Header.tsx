13b5d574d9a06f5f9977ae92936d5a6538e8f158
Aapeli <aapeli.haanpuu@hoxhunt.com>
2024-02-27 09:43:07 +0200

    Make panels resizable

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ type HeaderProps = {
 };
 export const Header: FC<HeaderProps> = ({ settings, setSettings }) => {
   return (
-    <div className="container mx-auto px-4 md:px-6 lg:px-8">
+    <div className="w-full px-4 md:px-6 lg:px-8">
       <header className="flex h-20 w-full shrink-0 items-center px-4 md:px-6">
         <h1 className="scroll-m-20 text-2xl font-extrabold tracking-tight lg:text-3xl">
           MermaidGPT
